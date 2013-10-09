96c8d39047e41dd75ae3753b4caa5c551d7da931
saravanan666 <saravanan666@users.noreply.github.com>
2013-10-09 22:56:14 +0530

    Update desktop-notify.sh

--- desktop-notify.sh
+++ desktop-notify.sh
@@ -9,6 +9,8 @@ echo "`date` $0 $1 $2 $3 $4 $5 $6" >> ${PWD}/../logs/active-responses.log
 
 log=`/bin/cat /opt/ossec-hids/logs/alerts/alerts.log | /bin/grep -A 3 "$4" | tail -1`
 
+rule=`/bin/cat /opt/ossec-hids/logs/alerts/alerts.log | /bin/grep -A 2 "$4" | tail -1 | awk '{print $2}'`
+
 log=${log:16}
 
-DISPLAY=0:0 /usr/bin/notify-send "$log"
+DISPLAY=0:0 /usr/bin/notify-send "$rule $log"
