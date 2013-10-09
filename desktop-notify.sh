ed50a3a479b42bde41a3c9ad047fbfc32a80e6ae
saravanan666 <saravanan666@users.noreply.github.com>
2013-10-09 22:43:34 +0530

    Update desktop-notify.sh

--- desktop-notify.sh
+++ desktop-notify.sh
@@ -5,12 +5,10 @@ cd $LOCAL
 cd ../
 PWD=`pwd`
 
-echo "`date` $0 $1 $2 $3 $4 $5 $6 $7 $8" >> ${PWD}/../logs/active-responses.log
+echo "`date` $0 $1 $2 $3 $4 $5 $6" >> ${PWD}/../logs/active-responses.log
 
-log_entry=`/bin/cat /opt/ossec-hids/logs/alerts/alerts.log | /bin/grep "$4" | tail -1`
+log=`/bin/cat /opt/ossec-hids/logs/alerts/alerts.log | /bin/grep -A 3 "$4" | tail -1`
 
-tmp="OSSEC HIDS $log_entry"
-
-log=`echo $tmp | sed 's/->//g'`
+log=${log:16}
 
 DISPLAY=0:0 /usr/bin/notify-send "$log"
